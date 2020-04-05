929429adec880ab4c76cd9ec08aa94c706075f38
Serg Klukvin <klukvin@ukr.net>
2020-04-05 20:07:21 +0300

    end

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let count;
+let template;
+let reg = /%/gi;
+
+for (; true; ) {
+  template = prompt("Enter template");
+
+  if (template.includes("%") === false) {
+    alert("Incorrect");
+    continue;
+  } else break;
+}
+
+count = template.match(reg).length;
+
+function result(count, template) {
+  let newStr = "";
+  let arr = [];
+
+  for (let i = 1; i <= count; i++) {
+    let tmp = prompt(`Enter element #${i}`);
+    arr.push(tmp);
+  }
+
+  let arrReplace = [];
+
+  for (let i = 1; i <= arr.length; i++) {
+    arrReplace.push(new RegExp(`%${i}`));
+  }
+
+  newStr = template.replace(arrReplace[0], arr[0]);
+
+  for (let i = 1; i < arr.length; i++) {
+    newStr = newStr.replace(arrReplace[i], arr[i]);
+  }
+
+  return `Parameteres: ${arr}<br> Result: ${newStr}`;
+}
+
+document.write(`Template: ${template}<br>${result(count, template)}`);
